97fd19c8d911cfdd4b8019b18d42c3ee24d07471
Shaskia Putri Devi <shaskiapdv@gmail.com>
2026-06-10 16:06:45 +0700

    Update vite.config.js

--- pokeArena/vite.config.js
+++ pokeArena/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "reactMakers/pokeArena/",
+  base: "/reactMakers/pokeArena/",
   plugins: [react()],
 
   server: {
